1f9dac18901f709083e0092abb1bc9b8c404ef16
philly <sonagi.philly@gmail.com>
2016-11-07 18:19:14 +0900

    add ndarray class

---
+++ numpy-csharp/NdArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace philly.numpy
+{
+    public class NdArray<T>
+    {
+        private List<ArrayList<T>> _list = new List<ArrayList<T>>();
+    }
+
+    internal class ArrayList<T>
+    {
+        private List<ArrayList<T>> _list;
+
+        protected ArrayList()
+        {
+            _list = new List<ArrayList<T>>();
+        }
+
+        public ArrayList(Array array) : this()
+        {
+            if(array.Rank >= 1)
+            {
+                for (int i = 0; i < array.GetLength(1); i++)
+                {
+                    Array ary = (Array)array.GetValue(i);
+                    _list.Add(new ArrayList<T>(array));
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    internal class ElementList<T> : ArrayList<T>
+    {
+        public ElementList(Array array) : base()
+        {
+
+        }
+    }
+}
